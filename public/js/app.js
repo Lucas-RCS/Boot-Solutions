972d9381e57acc056b5fcec56c042e3ef33318cf
Lucas-RCS <lucasribeiro2216@gmail.com>
2023-05-13 19:07:19 -0300

    atualização

--- public/js/app.js
+++ public/js/app.js
@@ -1,53 +1,41 @@
-// Scroll suave para link interno
-$('nav a').click(function(e){
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			menuHeight = $('nav').innerHeight(),
-			targetOffset = $(id).offset().top;
-	$('html, body').animate({
-		scrollTop: targetOffset - menuHeight
-	}, 500);
-});
-
-// Debounce do Lodash
-debounce = function(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-// Animação ao Scroll
-(function(){
-	var $target = $('.anime'),
-			animationClass = 'anime-start',
-			offset = $(window).height() * 3/4;
-
-	function animeScroll() {
-		var documentTop = $(document).scrollTop();
-
-		$target.each(function(){
-			var itemTop = $(this).offset().top;
-			if (documentTop > itemTop - offset) {
-				$(this).addClass(animationClass);
-			} else {
-				$(this).removeClass(animationClass);
-			}
-		});
+// Função para animação de scroll suave
+function smoothScroll(target, duration) {
+	var target = document.querySelector(target);
+	var targetPosition = target.getBoundingClientRect().top;
+	var startPosition = window.pageYOffset;
+	var distance = targetPosition - startPosition;
+	var startTime = null;
+  
+	function animation(currentTime) {
+	  if (startTime === null) startTime = currentTime;
+	  var timeElapsed = currentTime - startTime;
+	  var run = ease(timeElapsed, startPosition, distance, duration);
+	  window.scrollTo(0, run);
+	  if (timeElapsed < duration) requestAnimationFrame(animation);
 	}
-
-	animeScroll();
-
-	$(document).scroll(debounce(function(){
-		animeScroll();
-	}, 200));
-})();
-
+  
+	function ease(t, b, c, d) {
+	  t /= d / 2;
+	  if (t < 1) return c / 2 * t * t + b;
+	  t--;
+	  return -c / 2 * (t * (t - 2) - 1) + b;
+	}
+  
+	requestAnimationFrame(animation);
+  }
+  
+  // Selecionar todos os links de navegação
+  var links = document.querySelectorAll('nav a');
+  
+  // Adicionar evento de clique para cada link
+  links.forEach(function(link) {
+	link.addEventListener('click', function(e) {
+	  e.preventDefault();
+	  var href = this.getAttribute('href');
+	  if (href.startsWith('#')) {
+		smoothScroll(href, 1000);
+	  } else {
+		window.location.href = href;
+	  }
+	});
+  });
