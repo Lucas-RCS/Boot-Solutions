59aa083b2a7663cea37cb805cf1311afe294fedb
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2023-06-13 13:57:27 -0300

    backend

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\borracharia;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+
+Route::get('/homepage', [borracharia::class, 'homepage']);
+
+Route::resource('borracharia', UserController::class);
+
+Route::get('/login', function() {
+    return view('login');
+});
