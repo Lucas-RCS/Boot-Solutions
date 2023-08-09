abed67563872dcd3e2fb5c516462570ba09be5f2
Lucas Ribeiro <lucasribeiro2216@gmail.com>
2023-08-09 07:56:56 -0300

    update sidebar

--- src/views/assets/components/sideBar/sideBar.tsx
+++ src/views/assets/components/sideBar/sideBar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './sideBar.scss';
-import { BookmarksSimple, ChartBar, ChatCircleDots, Folders, GearSix, List, MagnifyingGlass, ShoppingCart, SquaresFour, Users } from '@phosphor-icons/react';
+import { ArrowCircleLeft, BookmarksSimple, ChartBar, ChatCircleDots, Folders, GearSix, List, MagnifyingGlass, ShoppingCart, SignOut, SquaresFour, Users } from '@phosphor-icons/react';
 
 
 const Sidebar: React.FC = () => {
@@ -46,68 +46,68 @@ const Sidebar: React.FC = () => {
     return (
         <div className="sidebar">
             <div className="logo_details">
-                <i className="bx icons"><List size={32} weight="duotone" /></i>
+                <i className="bx icons"><List size={24} weight="duotone" /></i>
                 <div className="logo_name">BootSolutions</div>
-                <i className="bx bx-menu" id="btn"></i>
+                <i className="bx bx-menu icons" id="btn"><ArrowCircleLeft size={24} weight="duotone" /></i>
             </div>
             <ul className="nav_list">
                 <li>
-                    <i className="bx bx-search icons"><MagnifyingGlass size={32} weight="duotone" /></i>
+                    <i className="bx bx-search icons"><MagnifyingGlass size={24} weight="duotone" /></i>
                     <input type="text" placeholder="Procurar..." />
                     <span className="tooltip">Procurar</span>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx icons"><SquaresFour size={32} weight="duotone" /></i>
+                        <i className="bx icons"><SquaresFour size={24} weight="duotone" /></i>
                         <span className="links_name">Dashboard</span>
                     </a>
                     <span className="tooltip">Dashboard</span>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx icons"><Users size={32} weight="duotone" /></i>
+                        <i className="bx icons"><Users size={24} weight="duotone" /></i>
                         <span className="links_name">Usuário</span>
                     </a>
                     <span className="tooltip">Usuário</span>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx icons"><ChatCircleDots size={32} weight="duotone" /></i>
+                        <i className="bx icons"><ChatCircleDots size={24} weight="duotone" /></i>
                         <span className="links_name">Notificações</span>
                     </a>
                     <span className="tooltip">Notificações</span>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx icons"><ChartBar size={32} weight="duotone" /></i>
+                        <i className="bx icons"><ChartBar size={24} weight="duotone" /></i>
                         <span className="links_name">Gráficos</span>
                     </a>
                     <span className="tooltip">Gráficos</span>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx icons"><Folders size={32} weight="duotone" /></i>
+                        <i className="bx icons"><Folders size={24} weight="duotone" /></i>
                         <span className="links_name">Arquivos</span>
                     </a>
                     <span className="tooltip">Arquivos</span>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx icons"><ShoppingCart size={32} weight="duotone" /></i>
+                        <i className="bx icons"><ShoppingCart size={24} weight="duotone" /></i>
                         <span className="links_name">Carrinho</span>
                     </a>
                     <span className="tooltip">Carrinho</span>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx icons"><BookmarksSimple size={32} weight="duotone" /></i>
+                        <i className="bx icons"><BookmarksSimple size={24} weight="duotone" /></i>
                         <span className="links_name">Favoritos</span>
                     </a>
                     <span className="tooltip">Favoritos</span>
                 </li>
                 <li>
                     <a href="#">
-                        <i className="bx icons"><GearSix size={32} weight="duotone" /></i>
+                        <i className="bx icons"><GearSix size={24} weight="duotone" /></i>
                         <span className="links_name">Configurações</span>
                     </a>
                     <span className="tooltip">Configurações</span>
@@ -116,11 +116,11 @@ const Sidebar: React.FC = () => {
                     <div className="profile_details">
                         <img src="./img/logo_gree.png" alt="profileImg" />
                         <div className="name_job">
-                            <div className="name">Prem Shahi</div>
-                            <div className="job">Web designer</div>
+                            <div className="name">Boot Solutions</div>
+                            <div className="job">Developer</div>
                         </div>
                     </div>
-                    <i className="" id="log_out"></i>
+                    <i className="" id="log_out"><SignOut size={24} weight="duotone" /></i>
                 </li>
             </ul>
         </div>
